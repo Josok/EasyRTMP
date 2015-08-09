cb22f0f030558d1bfe8e317b6648a7deff45bbf4
Babosa <mysunpany@gmail.com>
2015-08-09 19:05:28 +0800

    Init EasyRTMP

---
+++ EasyRTMP_RTSP/main.cpp
@@ -0,0 +1,104 @@
+/*
+	Copyright (c) 2013-2014 EasyDarwin.ORG.  All rights reserved.
+	Github: https://github.com/EasyDarwin
+	WEChat: EasyDarwin
+	Website: http://www.EasyDarwin.org
+*/
+#define _CRTDBG_MAP_ALLOC
+#include <stdio.h>
+#include "EasyRTMPAPI.h"
+#include "EasyNVSourceAPI.h"
+#include <winsock2.h>
+
+
+#define RTSPURL "rtsp://admin:admin@192.168.1.106/"
+
+#define SRTMP "rtmp://121.40.50.44/oflaDemo/aaa"
+
+Easy_RTMP_Handle rtmpHandle = 0;
+Easy_NVS_Handle fNVSHandle = 0;
+static unsigned int tsTimeStampMSsec = 0;
+
+/* NVSource从RTSPClient获取数据后回调给上层 */
+int CALLBACK __NVSourceCallBack( int _chid, int *_chPtr, int _mediatype, char *pbuf, NVS_FRAME_INFO *frameinfo)
+{
+	if (NULL != frameinfo)
+	{
+		if (frameinfo->height==1088)		frameinfo->height=1080;
+		else if (frameinfo->height==544)	frameinfo->height=540;
+	}
+	bool bRet = false;
+
+	if (_mediatype == MEDIA_TYPE_VIDEO)
+	{
+		tsTimeStampMSsec += 40;
+		if(frameinfo && frameinfo->length)
+		{
+			if( frameinfo->type == FRAMETYPE_I)
+			{
+				/* 关键帧是SPS、PPS、IDR(均包含00 00 00 01)的组合,reserved1是sps结尾的偏移,reserved2是pps结尾的偏移 */
+				if(rtmpHandle == 0)
+				{
+					rtmpHandle = EasyRTMP_Session_Create();
+
+					bRet = EasyRTMP_Connect(rtmpHandle, SRTMP);
+					if (!bRet)
+					{
+						printf("Fail to EasyRTMP_Connect ...\n");
+					}
+
+					bRet = EasyRTMP_InitMetadata(rtmpHandle, (const char*)pbuf+4,
+						frameinfo->reserved1-4, (const char*)pbuf+frameinfo->reserved1+4 ,
+						frameinfo->reserved2 - frameinfo->reserved1 -4, 25, 8000);
+					if (!bRet)
+					{
+						printf("Fail to InitMetadata ...\n");
+					}
+				}
+				bRet = EasyRTMP_SendH264Packet(rtmpHandle, (unsigned char*)pbuf+frameinfo->reserved2+4, frameinfo->length-frameinfo->reserved2-4, true, tsTimeStampMSsec);
+				if (!bRet)
+				{
+					printf("Fail to EasyRTMP_SendH264Packet(I-frame) ...\n");
+				}
+			}
+			else
+			{
+				bRet = EasyRTMP_SendH264Packet(rtmpHandle, (unsigned char*)pbuf+4, frameinfo->length-4, false, tsTimeStampMSsec);
+				if (!bRet)
+				{
+					printf("Fail to EasyRTMP_SendH264Packet(P-frame) ...\n");
+				}
+			}				
+		}	
+	}
+	return 0;
+}
+
+int main()
+{
+
+	//创建NVSource
+	EasyNVS_Init(&fNVSHandle);
+
+	if (NULL == fNVSHandle) return 0;
+
+	unsigned int mediaType = MEDIA_TYPE_VIDEO;
+	//mediaType |= MEDIA_TYPE_AUDIO;	//换为NVSource, 屏蔽声音
+
+	EasyNVS_SetCallback(fNVSHandle, __NVSourceCallBack);
+	EasyNVS_OpenStream(fNVSHandle, 0, RTSPURL, RTP_OVER_TCP, mediaType, 0, 0, NULL, 1000, 0);
+
+	while(1)
+	{
+		Sleep(10);	
+	};
+
+    EasyRTMP_Session_Release(rtmpHandle);
+    rtmpHandle = 0;
+   
+	EasyNVS_CloseStream(fNVSHandle);
+	EasyNVS_Deinit(&fNVSHandle);
+	fNVSHandle = NULL;
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+## EasyRTMP ##
+
+**EasyRTMP**是什么？
+
+EasyRTMP是一个EasyDarwin配套使用，也可以单独使用的RTMP推送库，通过EasyRTMP我们就可以避免接触到稍显复杂的RTMP推送流程，只需要调用EasyRTMP的几个API接口，就能轻松、稳定地把流媒体音视频数据推送给Red5、Ngnix、crtmpserver等RTMP服务器：
+
+
+- **EasyRTMP_RTSP**：将RTSP/RTP数据获取到本地，再通过EasyRTMP推送到RTMP服务器；
+
+- **EasyRTMP_SDK**：将自己SDK回调的数据推送给RTMP服务器；
+
+
+### 调用过程
+![](http://www.easydarwin.org/skin/easydarwin/images/easyrtmp20150908.png)
+
+### 获取更多信息 ###
+
+邮件：[support@easydarwin.org](mailto:support@easydarwin.org) 
+
+WEB：[EasyDarwin.org](http://www.easydarwin.org)
